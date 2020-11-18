2c4d149d6fc4644d71df3d87dff8155955ee8440
Mariuszsu <47736300+Mariuszsu@users.noreply.github.com>
2020-11-18 17:00:34 +0100

    Delete Zadanie5.java

--- Ćwiczenie 1/Zadanie5.java
+++
@@ -1,9 +0,0 @@
-public class Zadanie5
-{
-    public static void main(String[] args)
-    {
-        System.out.println(" ____");
-        System.out.println("|Java|");
-        System.out.println(" ____");
-    }
-}
