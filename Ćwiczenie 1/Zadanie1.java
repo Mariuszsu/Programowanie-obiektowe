50e1b07d7155fddb17b2af74e2b4ef89023a0142
Mariuszsu <47736300+Mariuszsu@users.noreply.github.com>
2020-11-18 16:59:52 +0100

    Delete Zadanie1.java

--- Ćwiczenie 1/Zadanie1.java
+++
@@ -1,8 +0,0 @@
-public class Zadanie1
-{
-    public static void main(String[] args)
-    {
-      System.out.println("Suma dni pierwszych trzech miesięcy dowolnego roku przestpnego:");
-      System.out.println(31+29+31);
-    }
-}
